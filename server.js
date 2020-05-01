7969336dda2691ad2bb217c1bb30b87db3934669
Ray Maldonado <rm01964@rmn.com>
2020-05-01 17:11:17 -0500

    Initial Commit

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 app.get('/', (req, resp) => resp.send('API Running - so cool'));
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
